7542f85ebae9dffda3ee7f7d5d7b12399f742594
larva <nguyenthanhcong12gtm1@gmail.com>
2023-04-08 12:55:00 +0700

    Update keyboard blink programs.

--- print/blink.c
+++ print/blink.c
@@ -8,13 +8,19 @@
 #include <linux/vt_kern.h> /* For fg_console. */
 #include <linux/console_struct.h> /* For vc_cons. */
 
+#define ALL_LEDS_ON 0x07
+#define RESTORE_ALL_LEDS 0xFF
+
 extern int fg_console; /* Current virtual console. */
 extern struct vc vc_cons [MAX_NR_CONSOLES];
+extern unsigned long volatile jiffies;
 
 static struct tty_driver *_driver = NULL;
 static struct timer_list _timer;
+static unsigned long _led_status = 0;
 
-void _callback(struct timer_list *timer);
+static inline void _reset_timer(void);
+static void _callback(struct timer_list *timer);
 
 static int __init _blink_init(void)
 {
@@ -25,13 +31,25 @@ static int __init _blink_init(void)
 
     for (i = 0; i < MAX_NR_CONSOLES; i++)
     {
+        if (vc_cons[i].d == NULL)
+        {
+            break;
+        }
+
+        pr_info("%s(): console[%i/%i] #%i, tty %p\n",
+                __FUNCTION__,
+                i,
+                MAX_NR_CONSOLES,
+                vc_cons[i].d->vc_num,
+                (void *)vc_cons[i].d->port.tty);
     }
 
+    // Get driver of current virtual console tty.
     _driver = vc_cons[fg_console].d->port.tty->driver;
     
     timer_setup(&_timer, _callback, 0);
 
-    _timer.expires = jiffies + HZ / 5;
+    _reset_timer();
 
     return 0;
 }
@@ -40,11 +58,49 @@ static int __init _blink_init(void)
 static void __exit _blink_exit(void)
 {
     pr_info("%s(): invoked.\n", __FUNCTION__);
+    del_timer(&_timer);
+    if (_driver != NULL)
+    {
+        (_driver->ops->ioctl)(vc_cons[fg_console].d->port.tty, KDSETLED, RESTORE_ALL_LEDS);
+    }
 }
 
-void _callback(struct timer_list *timer)
+static inline void _reset_timer(void)
 {
+    /* The global variable `jiffies` holds the number of ticks
+     * that have occurred since the system booted.
+     * On boot, the kernel initializes the variable to zero,
+     * and it is incremented by one during each timer interrupt.
+     * Thus, because there are HZ timer interrupts in a second,
+     * there are HZ jiffies in a second.
+     * 
+     * The system uptime is therefore jiffies/HZ seconds.
+     */
+    _timer.expires = jiffies + HZ / 5;
+    add_timer(&_timer);
+}
 
+
+static void _callback(struct timer_list *timer)
+{
+    pr_info("%s(): invoked.\n", __FUNCTION__);
+
+    /* Switch LED state. */
+    if (_led_status == ALL_LEDS_ON)
+    {
+        _led_status = RESTORE_ALL_LEDS;
+    } else
+    {
+        _led_status = ALL_LEDS_ON;
+    }
+
+    if (_driver != NULL)
+    {
+        pr_info("%s(): switching led state.\n", __FUNCTION__);
+        (_driver->ops->ioctl)(vc_cons[fg_console].d->port.tty, KDSETLED, _led_status);
+    }
+
+    _reset_timer();
 }
 
 
