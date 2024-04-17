b4d811430311423b4957078dea678dadd49315d5
larva <nguyenthanhcong12gtm1@gmail.com>
2024-04-17 10:10:31 -0700

    Implement mmap() userspace test.

---
+++ qemu/usr/mmap.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#define BAR_0_LENGTH (1024 * 1024)   // 1 MB.
+
+int main()
+{
+    int fd = open("/dev/c_pci_dev", O_RDWR);
+    if (fd < 0) {
+        printf("Cannot open device file\n");
+        return -1;
+    }
+
+    /* Addr is NULL, then the kernel chooses the (page-aligned)
+       address at which to create the mapping. */
+    uint8_t *pci_dev_bar0_base = mmap(NULL,
+                                      BAR_0_LENGTH,
+                                      PROT_READ | PROT_WRITE,
+                                      MAP_PRIVATE,
+                                      fd,
+                                      0);
+    if (pci_dev_bar0_base == MAP_FAILED) {
+        printf("Failed to map with PCI BAR 0\n");
+        close(fd);
+        return -1;
+    }
+
+    printf("mmap() success!.\n");
+
+    munmap(pci_dev_bar0_base, BAR_0_LENGTH);
+    close(fd);
+
+    return 0;
+}
