20aa80e93a2dd53d46a4e96975dae252f59725c6
LaVa264 <nguyenthanhcong12gtm1@gmail.com>
2024-05-16 11:18:37 +0700

    add creation thread example.

---
+++ usr/examples/lip/threads.c
@@ -0,0 +1,25 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+void* start_routine(void *arg)
+{
+    printf("start_routine\n");
+}
+
+int main()
+{
+    pthread_t thread;
+    pthread_attr_t attr = {0};
+    int result = 0;
+
+    result = pthread_create(&thread, &attr, start_routine, NULL);
+    if (result != 0)
+    {
+        printf("Failed to create pthread: %d\n", result);
+        exit(EXIT_FAILURE);
+    }
+
+    usleep(100);
+}
